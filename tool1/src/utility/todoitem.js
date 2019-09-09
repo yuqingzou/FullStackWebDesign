b11e129937542a1843ecc9c9f7d4acfad5851667
yuqingzou <yisubai@icloud.com>
2019-09-08 23:49:59 -0400

    inital the todolistcard parts

---
+++ tool1/src/utility/todoitem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class todoitem extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            isSelected:false
+        }
+    }
+
+
+}
+
+export default todoitem
