7b349fc6abbcad3b2b9ad2aa6da67acb3bab025b
yuqingzou <yisubai@icloud.com>
2019-09-17 23:26:57 -0400

    adding the router and test for useage

--- tool1/src/index.js
+++ tool1/src/index.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+//add component for router
+import RegisterForm from "./utility/registerform";
+import MemeGenerator from "./utility/MemeGenerator";
+import Todolist from "./utility/todolist";
+import Testone from "./utility/Testone";
+import { Route, Router, hashHistory, browserHistory } from "react-router";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let routes = (
+  <div className="app">
+    <Router>
+      <Route name="main">
+        <Route name="home" path="/" component={App} />
+        <Route name="register" path="/register" component={RegisterForm} />
+        <Route
+          name="jointeam"
+          path="/memegenerator"
+          component={MemeGenerator}
+        />
+        <Route name="testone" path="/testone" component={Testone}></Route>
+        <Route name="home" exact path="/todolist" component={Todolist} />
+      </Route>
+    </Router>
+  </div>
+);
 
+ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(routes, document.getElementById("root"));
 
 serviceWorker.unregister();
