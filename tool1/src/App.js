2c2a9064f25f76423077950de74b2cb81a399af2
yuqingzou <yisubai@icloud.com>
2019-09-19 20:58:58 -0400

    router now  works

--- tool1/src/App.js
+++ tool1/src/App.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React from "react";
 // import './App.css';
-import Todolist from './utility/todolist';
-import RegisterForm from './utility/registerform';
-import MemeGenerator from './utility/MemeGenerator'
+import Todolist from "./utility/todolist";
+import RegisterForm from "./utility/registerform";
+import MemeGenerator from "./utility/MemeGenerator";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header>here is the thing</header>
-      <Todolist/>
-      <br/>
-      <RegisterForm/>
-      <br/>
-      <MemeGenerator/>
-    </div>
+    <Router>
+      <div className="App">
+        <header
+          style={{
+            background:
+              "url(https://images.sharefaith.com/images/3/1507570813956_36/img_mouseover3.jpg)"
+          }}
+        >
+          here is the thing
+        </header>
+        <br />
+        <Link to="/todolist">To do list</Link>
+        <br />
+        <Link to="/memegenerator">Meme Generator</Link>
+        <br />
+        <Link to="/regiestorform">Register Form</Link>
+      </div>
+      <Route name="home" exact path="/todolist" component={Todolist} />
+      <Route
+        name="meme"
+        exact
+        path="/memegenerator"
+        component={MemeGenerator}
+      />
+      <Route
+        name="regist"
+        exact
+        path="/regiestorform"
+        component={RegisterForm}
+      />
+    </Router>
   );
 }
 
