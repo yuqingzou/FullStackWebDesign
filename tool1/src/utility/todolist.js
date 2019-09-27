cae9a5aae929cdd0b92825fd1d989ec7d87a42dd
meowpasi <ted@teds-MacBook-Pro.local>
2019-09-26 23:30:12 -0400

    transfer originally hardcode content to redux stire

--- tool1/src/utility/todolist.js
+++ tool1/src/utility/todolist.js
@@ -1,40 +1,12 @@
 import React from 'react'
-import ReactDOm from 'react-dom'
 import Todoitem from './todoitem'
+import store from '../store'
 // import Exmapletododate from './exmapletododate.js'
 
 class Todolist extends React.Component  {
     constructor(){
         super()
-        this.state = {
-            tododata:[
-                {
-                    id: 1,
-                    text: "Take out the trash",
-                    completed: true
-                },
-                {
-                    id: 2,
-                    text: "Grocery shopping",
-                    completed: false
-                },
-                {
-                    id: 3,
-                    text: "Clean gecko tank",
-                    completed: false
-                },
-                {
-                    id: 4,
-                    text: "Mow lawn",
-                    completed: true
-                },
-                {
-                    id: 5,
-                    text: "Catch up on Arrested Development",
-                    completed: false
-                }
-            ]
-        }
+        this.state = store.getState();
         //all function that need change state need be in here
         this.clickChange = this.clickChange.bind(this)
     }
