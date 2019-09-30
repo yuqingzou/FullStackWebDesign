081bba8c8a8e63eccf7c848738f4491632aca630
yuqingzou <yisubai@icloud.com>
2019-09-29 23:43:47 -0400

    removed old version setstate method all redux store now

--- tool1/src/store/reducer.js
+++ tool1/src/store/reducer.js
@@ -1,33 +1,40 @@
 const defaultState = {
-    
-    tododata:[
+  tododata: [
     {
-        id: 1,
-        text: "Take out the trash",
-        completed: true
+      id: 1,
+      text: "Take out the trash",
+      completed: true
     },
     {
-        id: 2,
-        text: "Grocery shopping",
-        completed: false
+      id: 2,
+      text: "Grocery shopping",
+      completed: false
     },
     {
-        id: 3,
-        text: "Clean gecko tank",
-        completed: false
+      id: 3,
+      text: "Clean gecko tank",
+      completed: false
     },
     {
-        id: 4,
-        text: "Mow lawn",
-        completed: true
+      id: 4,
+      text: "Mow lawn",
+      completed: true
     },
     {
-        id: 5,
-        text: "Catch up on Arrested Development",
-        completed: false
+      id: 5,
+      text: "Catch up on Arrested Development",
+      completed: false
     }
-]};
+  ]
+};
 
-export default (state = defaultState,action) =>{
-    return state;
-}
+export default (state = defaultState, action) => {
+  if (action.type === "click_done") {
+    //copy a previousa state instance
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.tododata = action.value;
+    return newState;
+  }
+  //return default !!!!
+  return state;
+};
--- tool1/src/utility/todolist.js
+++ tool1/src/utility/todolist.js
@@ -1,49 +1,44 @@
-import React from 'react'
-import Todoitem from './todoitem'
-import store from '../store'
+import React from "react";
+import Todoitem from "./todoitem";
+import store from "../store";
 // import Exmapletododate from './exmapletododate.js'
 
-class Todolist extends React.Component  {
-    constructor(){
-        super()
-        this.state = store.getState();
-        //all function that need change state need be in here
-        this.clickChange = this.clickChange.bind(this)
-    }
+class Todolist extends React.Component {
+  constructor() {
+    super();
+    this.state = store.getState();
+    console.log(this.state);
+    //all function that need change state need be in here
+    this.clickChange = this.clickChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    store.subscribe(this.handleChange);
+  }
 
-    clickChange(id){
-        
-        this.setState(
-            prevSate => {
-                const updated = this.state.tododata.map(item =>{
-                    if (item.id === id){
-                        item.completed = !item.completed
-                    }
-                    return item
-                })
-                //in here need return a state which should be a js object
-                return{
-                    tododata:updated
-                }
-            })
+  clickChange(id) {
+    const updated = this.state.tododata.map(item => {
+      if (item.id === id) {
+        item.completed = !item.completed;
+      }
+      return item;
+    });
 
-        const action ={
-            type : 'change_input_value',
-            value: this.state
-        }
-        store.dispatch(action);    
-    }
+    const action = {
+      type: "click_done",
+      value: updated
+    };
+    store.dispatch(action);
+  }
 
-    render(){
-        const todoitems = this.state.tododata.map(items=><Todoitem key={items.id} content={items} clickChange={this.clickChange}/>)
-        return(
-            <div className="todoList">
-                {todoitems}                
-            </div>
-        )
-    }
+  handleChange() {
+    this.setState(store.getState());
+  }
 
+  render() {
+    const todoitems = this.state.tododata.map(items => (
+      <Todoitem key={items.id} content={items} clickChange={this.clickChange} />
+    ));
+    return <div className="todoList">{todoitems}</div>;
+  }
+}
 
-} 
-
-export default Todolist
+export default Todolist;
